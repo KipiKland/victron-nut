bf271e5fce9e564913dee3ce0cce717e6d88557e
KipiKLand <kipik@kipik.com>
2026-05-17 14:47:45 +0200

    dont install certs

--- install/data/victron-nut/initialize.sh
+++ install/data/victron-nut/initialize.sh
@@ -2,17 +2,17 @@
 
 echo "Run victron-nut initialize script ..."
 
-if [ ! -L /usr/share/ca-certificates/franz-certs ]; then
-	echo "Add franz-certs symlink ..."
-	ln -s /data/victron-nut/franz-certs /usr/share/ca-certificates/franz-certs
-	update-ca-certificates
-fi
+# if [ ! -L /usr/share/ca-certificates/franz-certs ]; then
+# 	echo "Add franz-certs symlink ..."
+# 	ln -s /data/victron-nut/franz-certs /usr/share/ca-certificates/franz-certs
+# 	update-ca-certificates
+# fi
 
-if ! grep -q "franz-certs/reiter-edv-ca.crt" /etc/ca-certificates.conf; then
-	echo "Add reiter-edv-ca to ca-certificates ..."
-	echo "franz-certs/reiter-edv-ca.crt" >> /etc/ca-certificates.conf
-	update-ca-certificates
-fi
+# if ! grep -q "franz-certs/reiter-edv-ca.crt" /etc/ca-certificates.conf; then
+# 	echo "Add reiter-edv-ca to ca-certificates ..."
+# 	echo "franz-certs/reiter-edv-ca.crt" >> /etc/ca-certificates.conf
+# 	update-ca-certificates
+# fi
 
 if [ ! -L /service/victron-nut ]; then
 	echo "Add victron-nut service ..."
